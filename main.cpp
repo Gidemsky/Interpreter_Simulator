9b4f849f8d45e970162955f3c70a1b885bc06d70
Gideon <gidemsky26@gmail.com>
2018-12-14 00:53:46 +0200

    add david the borther

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World to david the brother!" << std::endl;
+    std::cout << "Hello, World to david the brother from noam!" << std::endl;
     return 0;
 }
