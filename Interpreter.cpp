9581575539ceb92d962964e921b754c0163e5200
Gideon <gidemsky26@gmail.com>
2018-12-14 14:12:54 +0200

    constructor bulided

--- Interpreter.cpp
+++ Interpreter.cpp
@@ -3,3 +3,68 @@
 //
 
 #include "Interpreter.h"
+
+
+Interpreter::Interpreter(){
+    fileLoaded = false;
+}
+
+Interpreter::Interpreter(fstream flightCode){
+    fileLoaded = false;
+
+}
+
+void Interpreter::loadFileData(fstream *dataFile, bool *isLoaded) {
+    vector <string> lineFilds;
+    string line;
+    dataFile->open(fileName);
+    if (!dataFile->is_open()) {
+        dataFile->open(fileName, fstream::out);
+        if (!dataFile->is_open()) {
+            throw "ERROR: CAN'T OPEN THE FILE";
+        }
+        dataFile->close();
+        return;
+    }
+    if (!*isLoaded) {
+        while (getline(*dataFile, line)) {
+            lineFilds = lineParser(line, " | ");
+            enterObjData(lineFilds, section);
+        }
+        if (section == PLANE) {
+            this->generator.setPlanes(static_cast<int>(companyFleet.size()));
+        } else if (section == EMPLOYEE) {
+            this->generator.setEmployees(static_cast<int>(employeeMap.size()));
+            for (int i = 1; i <= employeeMap.size(); i++) {
+                employeeMap[to_string(i)]->setEmployer(employeeMap[to_string(i)]);
+            }
+        } else if (section == FLIGHT) {
+            this->generator.setFlights(static_cast<int>(FlightsMap.size()));
+        }
+        //mark the files as readed
+        *isLoaded = true;
+    }
+    dataFile->close();
+    if (section == SCHEDUALE) {
+        for (int i = 1; i <= fleetSchedule.size(); i++) {
+            if (fleetSchedule[to_string(i)] == "") {
+                continue;
+            }
+            companyFleet[fleetSchedule[to_string(i)]]->
+                    setOccupiedDatesList(FlightsMap[to_string(i)]->getDate());
+        }
+    } else if (section == EMPLOYEE_SCHEDULE) {
+        if (FlightsMap.size() != 0 && crewSchedule.size() != 0) {
+            tempCrewSchedule = crewSchedule;
+            for (int i = 1; i <= FlightsMap.size(); i++) {
+                while (tempCrewSchedule.find(to_string(i)) != tempCrewSchedule.end()) {
+                    FlightsMap[to_string(i)]->setAssignedCrew(
+                            employeeMap[tempCrewSchedule.find(to_string(i))->second]);
+                    employeeMap[tempCrewSchedule.find(to_string(i))->second]->setOccupiedDatesList(
+                            FlightsMap[to_string(i)]->getDate());
+                    tempCrewSchedule.erase(tempCrewSchedule.find(to_string(i)));
+                }
+            }
+        }
+    }
+}
--- Interpreter.h
+++ Interpreter.h
@@ -5,10 +5,22 @@
 #ifndef SIMULATOR_INTERPRETER_H
 #define SIMULATOR_INTERPRETER_H
 
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+
+
+using namespace std;
 
 class Interpreter {
-
+    bool fileLoaded;
+    fstream simulatorUserCode;
+    string lexer;
+public:
+    Interpreter();
+    Interpreter(fstream flightCode);
+    void loadFileData(fstream *dataFile, bool *isLoaded);
 };
-
 
 #endif //SIMULATOR_INTERPRETER_H
