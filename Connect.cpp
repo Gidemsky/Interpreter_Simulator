f1d1891d8438def3b6eb5b771437f3e74dbd0261
Gideon <gidemsky26@gmail.com>
2018-12-27 16:48:16 +0200

    Connect ver 1.1

--- Connect.cpp
+++ Connect.cpp
@@ -4,6 +4,8 @@
 
 #include "Connect.h"
 #include <arpa/inet.h>
+#include <unistd.h>
+#include <iostream>
 
 extern Data data;
 
@@ -24,26 +26,34 @@ Connect::Connect(string ip_address, string port) {
 
 double Connect::execute() {
     int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0)  {
-        perror("ERROR: can not open the file!");
-        exit(1);
+        if (sock < 0) {
+            perror("ERROR: can not open the file!");
+            exit(1);
+        }
+
+        struct sockaddr_in server;
+        server.sin_family = AF_INET;
+        if (inet_pton(AF_INET, ip_address.c_str(), &server.sin_addr) <= 0)  {
+            perror("Error on IP pton");
+        }
+        server.sin_port = htons(port);
+
+        size_t server_len = sizeof(sockaddr_in);
+
+        std::cout << "debug" << endl;
+        memset(&server, 0, sizeof(server));
+
+        if (::connect(sock, (const sockaddr *) &server, server_len) < 0) {
+            perror("ERROR: can not connect to the host");
+            exit(1);
+        }
+
+    while(true) {
+        cout << "set" << endl;
+        // sock is the socket to the server
+        write(sock, "set /controls/flight/rudder 1\r\n", strlen("set /controls/flight/rudder 1\r\n"));
+        usleep(1);
     }
-
-    struct sockaddr_in server;
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = inet_addr(ip_address.c_str());
-    server.sin_port = htons(port);
-
-    size_t server_len = sizeof(sockaddr_in);
-
-    if (::connect(sock, (const sockaddr*)&server, server_len) < 0)    {
-        perror("ERROR: can not connect to the");
-        exit(1);
-    }
-
-    // sock is the socket to the server
-    write(sock, "set /controls/flight/rudder 1\r\n", strlen("set /controls/flight/rudder 1\r\n"));
-
     close(sock);
 
 }
